c8e293a583494f7b5ba90d504f5992e5e070d2d4
IT5706021630038 <wthnfbest@gmail.com>
2016-12-11 10:55:38 +0700

    frome prite

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.post('/webhook/', function (req, res) {
     if (event.message && event.message.text) {
       let text = event.message.text
       var location = event.message.text
-            var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=053b515dfaaa5b474e6701cdab179084'
+            var weatherEndpoint = 'http://api.openweathermap.org/data/2.5/weather?q=' +location+ '&units=metric&appid=ea5272e74853f242bc0efa9fef3dd9f3'
             request({
               url: weatherEndpoint,
               json: true
