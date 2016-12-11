0fc9ac63a9983f608e3dedac88abe5cbc701b5aa
IT5706021630038 <wthnfbest@gmail.com>
2016-12-11 11:22:09 +0700

    frome prite

--- server.js
+++ server.js
@@ -32,7 +32,8 @@ app.post('/webhook/', function (req, res) {
             }, function(error, response, body) {
               try {
                 var condition = body.main;
-                sendTextMessage(sender, "Today is " + condition.temp + " Celsius in " + location + " Max temperature " + condition.temp_max + " Min temperature " + condition.temp_min );
+                var weathe = body.weather;
+                sendTextMessage(sender, "Today is " + condition.temp + " Celsius in " + location + " Weather is " + weathe.main);
               } catch(err) {
                 console.error('error caught', err);
                 sendTextMessage(sender, "There was an error. Please enter location. Ex. Huahin");
