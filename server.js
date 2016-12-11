03e40b6ee69d905e541d3908496ee22ce856b87e
IT5706021630038 <wthnfbest@gmail.com>
2016-12-11 11:57:57 +0700

    frome prite

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.post('/webhook/', function (req, res) {
             }, function(error, response, body) {
               try {
                 var condition = body.main;
-                var cloud = body.clouds;
+                var cloud = body.clouds.all;
                 var realname = body.name;
                 var weathe = body.weather;
                 sendTextMessage(sender, "Today is " + condition.temp + " Celsius in " + realname + " Min temperature " + condition.temp_min +" Max temperature " + condition.temp_max +" cloud "+ cloud);
