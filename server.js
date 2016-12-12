48475c3f85186c48c86355220632d16d49f8bb88
IT5706021630038 <wthnfbest@gmail.com>
2016-12-13 03:03:24 +0700

    f

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ if (text === 'table'){
 
   function callback(options,error, response, body) {
 
-      sendTextMessage(sender, "เข้าcallbackแล้ว"+body.competition.name);
+      sendTextMessage(sender, "เข้าcallbackแล้ว");
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body);
       console.log(info.stargazers_count + " Stars");
