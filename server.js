e1996e7e913b74125e29a6b6de45ef89c8c79481
thanawat <tha_chookes@hotmail.com>
2016-12-13 19:48:49 +0700

    log response

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
 const app = express()
+const axios = require('axios')
 const token = 'EAAYzVx44hnYBAEiaPrinabDZBWmwC1rkal50SOWUpTiIt8ZChgu6BhNLQF3R8W2Qj24bNpCLEh3RbB4OBorD29649XGKQtXbgZCxkBdNpU2uvCFAZAXB2FyyMqW6ucKM3ueg0QttOnsWP5tbfGl2iYvfJrZAUNpd2mr1SPPicLQZDZD'
 app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -73,7 +74,7 @@ app.post('/webhook/', function (req, res) {
           sendTextMessage(sender, 'เข้าcallbackแล้ว')
           if (!error && response.statusCode == 200) {
             // var info = JSON.parse(body)
-            console.log(response)
+            console.log(response.body)
           }
         }
 
