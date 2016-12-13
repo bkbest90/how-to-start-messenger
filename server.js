e8120d3333da923670c5ab2c1e45c90150313d4b
IT5706021630038 <wthnfbest@gmail.com>
2016-12-13 23:16:09 +0700

    sendFirst test

--- server.js
+++ server.js
@@ -89,6 +89,7 @@ app.post('/webhook/', function (req, res) {
     if (event.postback) {
       let text = JSON.stringify(event.postback)
       sendTextMessage(sender, 'สวัสดี')
+      sendFirst (sender)
       continue
     }
   }
@@ -113,6 +114,45 @@ function sendTextMessage (sender, text) {
     }
   })
 }
+
+function sendFirst (sender) {
+  let messageData = {
+    'attachment': {
+      'type': 'template',
+      'payload': {
+
+          'buttons': [{
+            'type': 'web_url',
+            'url': 'https://www.messenger.com',
+            'title': 'web url'
+          }, {
+            'type': 'postback',
+            'title': 'table',
+            'payload': "table"
+          }]
+        }]
+      }
+    }
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending messages: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
+
+
+
 
 function sendGenericMessage (sender) {
   let messageData = {
