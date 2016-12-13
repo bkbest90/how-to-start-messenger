56e1b2e73b271ff7a7c4e3b626d4baf826afbe6b
IT5706021630038 <wthnfbest@gmail.com>
2016-12-13 08:05:45 +0700

    t

--- server.js
+++ server.js
@@ -77,7 +77,7 @@ if (text === 'table'){
 
   function callback(error, response, body) {
 
-      sendTextMessage(sender, "เข้าcallbackแล้ว"+ body.[0].competition.name);
+      sendTextMessage(sender, "เข้าcallbackแล้ว"+ body.leagueTable[0]);
     if (!error && response.statusCode == 200) {
       var info = JSON.parse(body);
       console.log(info.stargazers_count + " Stars");
