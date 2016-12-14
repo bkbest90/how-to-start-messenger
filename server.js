50b8eb9be7d68ba544456d673e870602d40515b3
IT5706021630038 <wthnfbest@gmail.com>
2016-12-15 00:46:04 +0700

    c

--- server.js
+++ server.js
@@ -192,7 +192,7 @@ function matches(sender){
    }
 */
             //  sendTextMessage(sender, JSON.parse(body)[0].outcome)
-            if(  JSON.parse(body)[0].outcom[] !== null ){
+            if(  JSON.parse(body)[0].outcom[0] !=== null ){
             sendTextMessage(sender, JSON.parse(body)[0].competition.name)
           }
     }
