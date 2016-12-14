a32f45b0be17451ae4018563e840aa81fbf08898
IT5706021630038 <wthnfbest@gmail.com>
2016-12-14 07:17:26 +0700

    t

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/webhook/', function (req, res) {
 
         premierleaguetable2(sender)
       }
-      if (payloadt === 'table1') {
+      if (payloadt === 'table2') {
         laligatable1(sender)
 
         laligatable2(sender)
