1104640826a92fe12fea152ad4d8e75c131a8677
IT5706021630038 <wthnfbest@gmail.com>
2016-12-22 13:41:53 +0700

    button

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.post('/webhook/', function (req, res) {
         sendTextMessage(sender, '**คำสั่งทั้งหมดของเรา**\nเมนูหลัก = กลับมาที่เมนูหลัก\nบอลวันนี้ = จะแสดงการแข่งขันของวันนี้\nบอลพรุ่งนี้ = จะแสดงการแข่งขันของวันพรุ่งนี้\nคำสั่ง = แสดงคำสั่งทั้งหมดอีกครั้ง')
       }
       if (text === 'asdf') {
-        console.log(sender);
+
         sendButtonMessage(sender)
 /*
        premierleagueTomorrow(sender)
@@ -6911,6 +6911,30 @@ function sendButtonMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: keypass },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      if (messageId) {
+        console.log("Successfully sent message with id %s to recipient %s",
+          messageId, recipientId);
+      } else {
+      console.log("Successfully called Send API for recipient %s",
+        recipientId);
+      }
+    } else {
+      console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
+    }
+  });
+}
 
 app.listen(app.get('port'), function () {
 console.log('running on port', app.get('port'))
