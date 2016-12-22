83e8aa6290fc795851013eed37764f7d6f7a40a2
IT5706021630038 <wthnfbest@gmail.com>
2016-12-22 14:53:20 +0700

    button

--- server.js
+++ server.js
@@ -53,11 +53,7 @@ app.post('/webhook/', function (req, res) {
       }
       if (text === 'asd') {
 
-        premierleagueTomorrow(sender)
-/*
-       premierleagueTomorrow(sender)
-
-*/
+        laligaTomorrow(sender)
 
       }
     }
